0f6877e9eeb2bd440ff2aaaea407442b4bc1d8d4
S.M.Owais Anas <165829540+dev372k@users.noreply.github.com>
2026-05-14 22:09:00 +0300

    feat "update config"

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    APP_NAME: str = "KV Store API"
+    APP_NAME: str = "Sample fast api"
     VERSION:str = "1.0.0"
     ENVIRONMENT: str = "development"
     LOG_LEVEL: str = "INFO"
