9d64bec827954021f6bbad267a04d14b8432737b
S.M.Owais Anas <165829540+dev372k@users.noreply.github.com>
2026-05-18 11:41:55 +0300

    release "v1.0.5"

---
+++ infrastructure/v2/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ami" {
+  description = "Ubuntu AMI"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+}
+
+variable "security_group_id" {
+  description = "Security group allowing SSH + port 3030"
+}
